58d14ce425b8fec1b718562a7bc75c5444fa8406
Zachary Elliott <contact@zell.io>
2021-05-26 15:22:16 -0400

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aptible-api",
-    version="0.2.0",
+    version="0.2.1",
     author="Zachary Elliott",
     author_email="zellio@trialspark.com",
     description="Object Oriented interface for Aptible API",
